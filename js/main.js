45cbf314a273eebc77a77c7085001b0e3c62f750
EC2 Default User <ec2-user@ip-172-31-43-17.us-east-2.compute.internal>
2018-10-19 07:02:24 +0000

    logic finalized

--- js/main.js
+++ js/main.js
@@ -26,25 +26,43 @@ angular.module('News', ['ui.router'])
     .factory('choicesBox', [function() {
         var o = {
             choices: [
-                [{ title: "have rainy weather?", image: "Some Image Here", fun: -1 }, { title: "have sunny weather?", image: "Some other image", fun: 1 }],
-                [{ title: "eat Skittles?", image: "Some Image Here", fun: 1 }, { title: "eat fancy chocolate?", image: "Some other image", fun: -1 }],
-                [{ title: "own a nice cabin?", image: "Some Image Here", fun: 1 }, { title: "own a nice house?", image: "Some other image", fun: -1 }],
-                [{ title: "go on a hike?", image: "Some Image Here", fun: -1 }, { title: "go to the beach?", image: "Some other image", fun: 1 }],
-                [{ title: "shop for sunglasses?", image: "Some Image Here", fun: 1 }, { title: "shop for watches?", image: "Some other image", fun: -1 }],
-                [{ title: "go to a concert?", image: "Some Image Here", fun: 1 }, { title: "go to a play?", image: "Some other image", fun: -1 }],
-                [{ title: "go to a party?", image: "Some Image Here", fun: 1 }, { title: "go to a classy restaurant?", image: "Some other image", fun: -1 }],
-                [{ title: "go out with friends?", image: "Some Image Here", fun: 1 }, { title: "stay in with friends?", image: "Some other image", fun: -1 }],
-                [{ title: "drink water?", image: "Some Image Here", fun: -1 }, { title: "drink soda?", image: "Some other image", fun: 1 }],
-                [{ title: "carve a pumpkin?", image: "Some Image Here", fun: -1 }, { title: "make a gingerbread house?", image: "Some other image", fun: 1 }],
-                [{ title: "go on a sleigh ride?", image: "Some Image Here", fun: 1 }, { title: "go on a hay ride?", image: "Some other image", fun: -1 }],
-                [{ title: "watch a football game?", image: "Some Image Here", fun: -1 }, { title: "play in a football game?", image: "Some other image", fun: 1 }],
+                [{ title: "have rainy weather?", image: "img/rain.jpeg", fun: -1 }, { title: "img/sun.jpg", image: "img/sun.jpg", fun: 1 }],
+                [{ title: "eat Skittles?", image: "img/skittles.jpg", fun: 1 }, { title: "eat fancy chocolate?", image: "img/chocolates.jpg", fun: -1 }],
+                [{ title: "own a nice cabin?", image: "img/cabin.jpg", fun: 1 }, { title: "own a nice house?", image: "img/house.jpg", fun: -1 }],
+                [{ title: "go on a hike?", image: "img/hike.jpg", fun: -1 }, { title: "go to the beach?", image: "img/beach.jpg", fun: 1 }],
+                [{ title: "shop for sunglasses?", image: "img/sunglasses.jpg", fun: 1 }, { title: "shop for watches?", image: "img/watches.jpeg", fun: -1 }],
+                [{ title: "go to a concert?", image: "img/concert.jpg", fun: 1 }, { title: "go to a play?", image: "img/play.jpg", fun: -1 }],
+                [{ title: "go to a party?", image: "img/party.jpg", fun: 1 }, { title: "go to a classy restaurant?", image: "img/restaurant.jpg", fun: -1 }],
+                [{ title: "go out with friends?", image: "img/out.jpg", fun: 1 }, { title: "stay in with friends?", image: "img/in.jpg", fun: -1 }],
+                [{ title: "drink water?", image: "img/water.jpg", fun: -1 }, { title: "drink soda?", image: "img/soda.jpg", fun: 1 }],
+                [{ title: "carve a pumpkin?", image: "img/pumpkin.jpg", fun: -1 }, { title: "make a gingerbread house?", image: "img/gingerbread.jpg", fun: 1 }],
+                [{ title: "go on a sleigh ride?", image: "img/sleigh.jpg", fun: 1 }, { title: "go on a hay ride?", image: "img/hay.jpg", fun: -1 }],
+                [{ title: "watch a football game?", image: "img/watches.jpeg", fun: -1 }, { title: "play in a football game?", image: "img/playing.jpg", fun: 1 }],
             ]
         };
         return o;
     }])
     .factory('funVal', [function() {
         var o = {
-            characterValue : 5
+            characterValue: 5
+        };
+        return o;
+    }])
+    .factory('characterBox', [function() {
+        var o = {
+            choices: [
+                { name: "Toby", src: "img/toby.jpg" },
+                { name: "Stanley", src: "img/stanley.jpg" },
+                { name: "Angela", src: "img/angela.jpg" },
+                { name: "Phyllis", src: "img/phyllis.jpg" },
+                { name: "Oscar", src: "img/oscar.jpg" },
+                { name: "Dwight", src: "img/dwight.jpg" },
+                { name: "Kevin", src: "img/kevin.jpg" },
+                { name: "Pam", src: "img/pam.jpg" },
+                { name: "Jim", src: "img/jim.jpg" },
+                { name: "Andy", src: "img/andy.jpg" },
+                { name: "Michael", src: "img/michael.png" },
+            ]
         };
         return o;
     }])
@@ -54,7 +72,7 @@ angular.module('News', ['ui.router'])
         'choicesBox',
         'funVal',
         function($scope, postFactory, choicesBox, funVal) {
-            $scope.posts = postFactory.choices;
+            $scope.posts = [];
             $scope.choices = choicesBox.choices;
             $scope.fun = funVal.characterValue;
             $scope.showResults = '';
@@ -63,44 +81,40 @@ angular.module('News', ['ui.router'])
                 if (len == 0) {
                     $scope.showResults = "Show Results";
                 }
-                var index = Math.floor(Math.random() * len);
-                $scope.posts.push({
-                    title: $scope.choices[index][0].title,
-                    image: $scope.choices[index][0].image,
-                    fun: $scope.choices[index][0].fun
-                });
-                $scope.posts.push({
-                    title: $scope.choices[index][1].title,
-                    image: $scope.choices[index][1].image,
-                    fun: $scope.choices[index][1].fun
-                });
-                $scope.title = '';
-                choicesBox.choices.splice(index,1);
-                
+                else {
+                    var index = Math.floor(Math.random() * len);
+                    $scope.posts[0] = {
+                        title: $scope.choices[index][0].title,
+                        image: $scope.choices[index][0].image,
+                        fun: $scope.choices[index][0].fun
+                    };
+                    $scope.posts[1] = {
+                        title: $scope.choices[index][1].title,
+                        image: $scope.choices[index][1].image,
+                        fun: $scope.choices[index][1].fun
+                    };
+                    $scope.title = '';
+                    choicesBox.choices.splice(index, 1);
+                    $scope.selectedOne = { src: $scope.posts[0].image, title: $scope.posts[0].title, fun: $scope.posts[0].fun }
+                    $scope.selectedTwo = { src: $scope.posts[1].image, title: $scope.posts[1].title, fun: $scope.posts[1].fun }
+                }
             };
-            $scope.selectChoice = function(post) {
-                $scope.fun += post.fun;
+            $scope.selectChoice = function(fun) {
+                $scope.fun += fun;
                 $scope.start();
             };
         }
     ])
-    .controller('PostCtrl', [
+    .controller('ResultsCtrl', [
         '$scope',
         '$stateParams',
         'postFactory',
-        function($scope, $stateParams, postFactory) {
-            $scope.post = postFactory.posts[$stateParams.id];
-            $scope.addComment = function() {
-                alert("here");
-                if ($scope.body === '') { return; }
-                $scope.post.comments.push({
-                    body: $scope.body,
-                    upvotes: 0
-                });
-                $scope.body = '';
-            };
-            $scope.incrementUpvotes = function(comment) {
-                comment.upvotes += 1;
-            };
+        'characterBox',
+        function($scope, $stateParams, postFactory, characterBox) {
+            $scope.characters = characterBox.choices;
+            var char = $stateParams.id;
+            char = char < 0 ? 0 : char;
+            char = char > 10 ? 10 : char;
+                $scope.character = { src: $scope.characters[char].src, name: $scope.characters[char].name }
         }
     ]);
